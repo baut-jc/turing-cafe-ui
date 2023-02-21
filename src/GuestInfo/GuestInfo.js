727d5451df5e144c55cf863d94e85a873103e12c
Jocelle Bautista <jcllebautista@gmail.com>
2023-02-21 13:41:52 -0500

    Display guest details to the website

--- src/GuestInfo/GuestInfo.js
+++ src/GuestInfo/GuestInfo.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './GuestInfo.css'
 
-export default function GuestInfo() {
+export default function GuestInfo({name, date, time, number}) {
   return (
-    <div>GuestInfo</div>
+    <div className='guestCard'>
+     <h3>{name}</h3>
+      <p>{date}</p>
+      <p>{time} pm</p>
+      <p>Number of people: {number}</p>
+      <button>Cancel</button>
+    </div>
   )
 }
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -6,14 +6,17 @@ export default function Reservations({reservations}) {
   const guestLists = reservations.map(guest => {
     console.log('guest', guest)
     return (
-      <div className='reservations'>
-        <GuestInfo />
-        <h1>{guest.name}</h1>
-        <p>{guest.date}</p>
-        <p>{guest.time}</p>
-        <p>{guest.number}</p>
-      </div>
+        <GuestInfo 
+          name={guest.name}
+          date={guest.date}
+          time={guest.time}
+          number={guest.number}
+        />
     )
   })
-  return guestLists
+  return (
+    <div className='reservations'>
+      {guestLists}
+    </div>
+  )
 }
